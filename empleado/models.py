5fbfc0577ec45698b3eecef88fd1d260d5a21060
Yahir <yahirrb343@gmail.com>
2023-11-11 19:29:41 -0600

    Campo telefono modificado

--- empleado/models.py
+++ empleado/models.py
@@ -4,12 +4,12 @@ from rest_framework import serializers
 # Create your models here.
 class Empleado(models.Model):
      nombre=models.TextField()
-     apellidos=models.TextField()
-     telefono=models.TextField()    
+     apellidos=models.TextField() 
      edad=models.IntegerField()
      sexo=models.CharField(max_length=9)
      correo=models.TextField()
-     es_activo= models.BooleanField(default=True)
+     es_activo= models.BooleanField(default=True),
+     usuario=models.TextField()
      
 class EmpleadoSerializer(serializers.ModelSerializer):
     class Meta:
